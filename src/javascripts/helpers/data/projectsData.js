0fe3b22732602a20831402479c87d896cacf5e07
Brooke Nemchak <bnemchak@gmail.com>
2020-07-12 13:39:32 -0500

    wip

---
+++ src/javascripts/helpers/data/projectsData.js
@@ -0,0 +1,14 @@
+const projects = [
+
+  {
+    title: 'Cool Project',
+    screenshot: 'http://gotoflashgames.com/files/file/033.jpg',
+    description: 'This is the best project', // A good project description includes 'the what', 'the why', and 'the how'.
+    technologiesUsed: 'HTML, CSS, Vanilla JavaScript, Version Control with Github',
+    available: true,
+    url: 'https://github.com/nss-evening-cohort-8/js-part-deux', // just use your GitHub link in this spot as well.
+    githubUrl: 'https://github.com/nss-evening-cohort-8/js-part-deux',
+  },
+];
+
+export default { projects };
