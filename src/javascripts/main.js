aa2ac8aecc4dadeb50c42e7143199ec3c87928e2
Brooke Nemchak <bnemchak@gmail.com>
2020-07-12 14:39:37 -0500

    firebase adjustments

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,9 +1,20 @@
+import firebase from 'firebase/app';
+import apiKeys from './helpers/apiKeys.json';
+import projectsData from './helpers/utils';
+import utils from './helpers/utils';
+
+import'bootstrap';
 import '../styles/main.scss';
 
-import projects from './components/projects';
-
-const init = () => {
-  projects.myProjects();
-};
-
-init();
+const createProjects = () => {
+  projectsData.getProjects()
+    .then((projects) => {
+      let domString = '<header>Projects</header>';
+      projects.forEach((project) => {
+        if (project.available === true) {
+          domString += '<div class="projectsPage">';
+          domString +=
+        }
+      })
+    })
+}
